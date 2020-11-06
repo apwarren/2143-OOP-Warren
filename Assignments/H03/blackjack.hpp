a0a37d57d4c625225e5f651ffe4c11cf4e03aecd
apwarren <70217207+apwarren@users.noreply.github.com>
2020-11-06 15:54:36 -0600

    Update blackjack.hpp

--- Assignments/H03/blackjack.hpp
+++ Assignments/H03/blackjack.hpp
@@ -379,6 +379,16 @@ Hand::Hand(){
 
 // It would be better to use the built in 
 // vector "sort". But ... oh well.
+/*
+*   Sort()
+*   The function originally compared i to j+1 and determined which card rank was higher,
+*   however, when it went to swap the two ranks, it would swap the card at j rather than the 
+*   card at j+1 because of how the index was set up. Rather than have j be equal to i and then
+*   compare i to j+1, it is better to already have j be 1 greater than i and loop through as such.
+*   When a card at index j is higher than the card at index i, then the swap will switch properly.
+*   This change keeps the sort a bubble sort but keeps i and j as their respective values rather than
+*   looking at the indexes above or below.
+*/
 void Hand::Sort(){
 
     // Index "i" 
