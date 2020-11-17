bd3ed5a948922823eefec04e289136eab3b58ccb
apwarren <allywarren7@gmail.com>
2020-11-16 20:16:29 -0600

    No comments still but it's good

--- Assignments/P02/BlackJack.hpp
+++ Assignments/P02/BlackJack.hpp
@@ -1,279 +1,141 @@
 #pragma once
-#include "Deck.hpp"
-#include "Player.hpp"
-#include "Dealer.hpp"
+#include "Card.hpp"
+#include "Container.hpp"
 #include <iostream>
-#include <vector>
-#include <iomanip>
-#include <chrono>
-#include <thread>
+#include <stdlib.h>
+#include <ctime>
 using namespace std;
-
-class BlackJack
+class Deck : public Container
 {
 private:
-  Deck CardDeck;
-  Player* player;
-  Dealer* dealer;
-  char playerOutput;
-  bool Quit;
-  int Activity;
-  bool playerMoveOn;
-  bool showDealerSum;
+	int OriginalSize;
 public:
-BlackJack(int decks);
-void Deal();
-void resetGame();
-void playerTurn();
-void dealerTurn();
-void noBust();
-bool quit();
-void Print();
-void PrintBeginning();
-};
-
- BlackJack :: BlackJack(int decks): CardDeck(decks * 52)
-  {
-    dealer = new Dealer(CardDeck.remove(), CardDeck.remove());
-		player = new Player(CardDeck.remove(), CardDeck.remove());
-    Activity = 3;
-    playerMoveOn = false;
-    showDealerSum = false;
-    Quit = false;
-  }
-  void BlackJack :: Deal()
-  {
-    if(player->Bankrupt())
-    {
-      cout << "\n\nYou Are Out Of Money!!!\nGoodBye!";
-      Quit = true;
-    }
-
-    else{
-    //Player starts game
-    player->Bet();
-    Print();
-    //Check for BlackJack
-     if(player->BlackJack())
-          {
-          if(dealer->BlackJack())
-          player->Tie();
-          else
-          {
-          Print();
-          cout << "BLACKJACK!!!!\n";
-          Activity = 0;
-          }
-          }
-    //If no BlackJack then continue playing until everyone is finished with hand
-    if(Activity > 0)
-    {    
-      while(player->isActive() && Activity > 2)
-        playerTurn();
-        //Dealer's turn
-      while(dealer->isActive() && Activity > 1)
-      {
-        
-        cout << "Dealer is Playing. . .\n";
-        chrono::milliseconds dura( 1000 );
-        this_thread::sleep_for( dura );
-        
-        dealerTurn();
-      }
-
-      if(Activity == 1) //Both Player and Dealer have Stood/ No Bust
-          noBust();
-
-    }//if Not bankrupt   
-  }
-    resetGame();
-}
-void BlackJack :: resetGame()
-{
-    delete dealer;
-    delete player;
-    Activity = 3;
-    dealer = new Dealer(CardDeck.remove(), CardDeck.remove());
-		player = new Player(CardDeck.remove(), CardDeck.remove());
-    showDealerSum = false;
-    playerMoveOn = false;
-}
-void BlackJack :: playerTurn()
-{
-          while(!playerMoveOn)
-          {
-          player->DoubleDown();
-          cout << "H: Hit | S: Stand | R: Show Deck | Q: Quit | A: Sort Hand" << endl;
-          cin >> playerOutput;
-          switch(playerOutput)
-          {
-            case 'D': case 'd':
-            player->chooseDD();
-            Print();
-             break;
-            case 'H': case 'h': 
-            player->Hit(CardDeck.remove());
-            Print();
-            playerMoveOn = true;
-            break;
-            case 'S': case 's':
-            player->Stand();
-            Activity--;
-            playerMoveOn = true;
-            break;
-            case 'A': case 'a':
-             player->sortHand();
-             Print();
-            break;
-            case 'R': case 'r':
-            CardDeck.PrintDeck();
-            cout << "Press enter any key and hit enter to return to game\n";
-            cin >> playerOutput;
-            Print();
-            break;
-            case 'Q': case 'q':
-            Activity = 0;
-            Quit = true;
-            playerMoveOn = true;
-            break;
-          }//end of switch(playerOutput)
-          }//player has made choice
-           if(player->Bust())
-          {
-          player->Stand();
-          Activity=0;
-          cout << "\nYou Busted!\tYou Lose!\n";
-          }
-          else
-          {
-            if(!Quit)
-            Print();
-            if(player->isActive())
-            cout << "Your Move.\n";
-          }
-          playerMoveOn = false;
-}
-void BlackJack :: dealerTurn()
-{
-      if(!dealer->Bust())
-      {
-         if(dealer->underHit())
-          {
-            dealer->Hit(CardDeck.remove());
-            if(dealer->Bust())
-            {
-            player->Win();
-            showDealerSum = true;
-            dealer->revealBlank(); //Flip over the first card
-            Print();   //Show Full Dealer Hand
-            cout << "Dealer Busts!\t You Win!\n";
-            Activity = 0;
-            }
-            else
-            {
-             Print(); 
-            }
-          }
-        else
-        {
-          dealer->Stand();
-          Activity--;
-        }
-      }
-      else{
-            player->Win();
-            showDealerSum = true;
-            dealer->revealBlank(); //Flip over the first card
-            Print();   //Show Full Dealer Hand
-            cout << "Dealer Busts!\t You Win!\n";
-            Activity = 0;
-      }
-}
-void BlackJack :: noBust()
-{
-  showDealerSum = true;
-        dealer->revealBlank(); //Flip over the first card
-        if(player->sum() > dealer->sum())// Player Wins
-        {
-          player->Win();
-          Print();
-          cout<< "YOU WIN!\n";
-        }
-        else if(player->sum() == dealer->sum()) //It's a tie
-        {
-          player->Tie();
-          Print();
-          cout << "It's a Tie!\n";
-
-        }
-        else //Player lost
-        {
-          Print();
-          cout << "Dealer Was Highest!\tYou Lose!\n";
-        }
-        Activity--;
-}
-bool BlackJack:: quit()
-{
-  return Quit;
-}
-    
+	Deck(): Container (52) //Initialize a deck of 52 cards
+	{
+    OriginalSize = 52;
+		Fill();//Fill Deck with cards
+    Shuffle();
+	}
+	Deck(int GivenSize): Container (GivenSize)
+	{
+    OriginalSize = GivenSize;
+		Fill();//Fill Deck with cards
+    Shuffle();
+	}
+	void Fill()
+	{
+		//Fill Deck with cards 
+		for(int i = 0; i < size(); i++)//Fills deck with initialized cards
+		{
+			cards[i]->setRank(i % 13); //Sets the Rank of the Card From Ace - King
+			cards[i]->setValue(i % 13); //Sets all card values from 1 - 10
+			cards[i]->setSuit( (i / 13) % 4); //Sets the suit from 1-4 each representing a suit
+		}
+	}//end of Fill()
   
-  void BlackJack :: Print()
-  {
-//Print out beginning of game
-cout << "\n\n\n\n\n\n\n\n\n";
-cout << "██████  ██       █████   ██████ ██   ██      ██  █████   ██████ ██   ██\n";
-cout << "██   ██ ██      ██   ██ ██      ██  ██       ██ ██   ██ ██      ██  ██\n";
-cout << "██████  ██      ███████ ██      █████        ██ ███████ ██      █████\n";
-cout << "██   ██ ██      ██   ██ ██      ██  ██  ██   ██ ██   ██ ██      ██  ██\n";
-cout << "██████  ███████ ██   ██  ██████ ██   ██  █████  ██   ██  ██████ ██   ██\n"; 
-cout << endl;
-cout << "  ___  ___   _   _    ___ ___\n";
-cout << " |   \\| __| /_\\ | |  | __| _ \\\n";
-cout << " | |) | _| / _ \\| |__| _||   /\n";
-cout << " |___/|___/_/ \\_\\____|___|_|_\\\n";
-cout << endl;
-dealer->printHand();
-if(showDealerSum)
-cout << "\t\tDealer Total: " << dealer->sum();
-cout << endl;
-cout << endl;
-cout << "  ___ _      ___   _____ ___\n";
-cout << " | _ \\ |    /_\\ \\ / / __| _ \\\n";
-cout << " |  _/ |__ / _ \\ V /| _||   /\n";
-cout << " |_| |____/_/ \\_\\_| |___|_|_\\\n";
-cout << endl;
-player->printHand();
-cout << "\t\tPlayer Total: "<< setw(2) << player->sum();
-cout << "\n\nBank: " << setw(4) << player->getBank();
-cout << "\n\n";
-}
-void BlackJack :: PrintBeginning()
-{
- cout << "██████  ██       █████   ██████ ██   ██      ██  █████   ██████ ██   ██\n";
-cout << "██   ██ ██      ██   ██ ██      ██  ██       ██ ██   ██ ██      ██  ██\n";
-cout << "██████  ██      ███████ ██      █████        ██ ███████ ██      █████\n";
-cout << "██   ██ ██      ██   ██ ██      ██  ██  ██   ██ ██   ██ ██      ██  ██\n";
-cout << "██████  ███████ ██   ██  ██████ ██   ██  █████  ██   ██  ██████ ██   ██\n"; 
-cout << endl;
-cout << "  ___  ___   _   _    ___ ___\n";
-cout << " |   \\| __| /_\\ | |  | __| _ \\\n";
-cout << " | |) | _| / _ \\| |__| _||   /\n";
-cout << " |___/|___/_/ \\_\\____|___|_|_\\\n";
-cout << endl;
-cout << endl;
-cout << "\n\n\n\n\n";
-cout << endl;
-cout << "  ___ _      ___   _____ ___\n";
-cout << " | _ \\ |    /_\\ \\ / / __| _ \\\n";
-cout << " |  _/ |__ / _ \\ V /| _||   /\n";
-cout << " |_| |____/_/ \\_\\_| |___|_|_\\\n";
-cout << endl;
-cout << "\n\n\n\n\n";
-cout<< endl;
-cout << "Total: --";
-cout << "\n\nBank: " << player->getBank();
-cout << "\n";
-cout << "\n\t\t| Press Any Key And Hit Enter To Begin Playing |" << endl;
-}
+  
+  Card* remove()
+	{
+    if(Size == 0) //Reset the deck if it has run out of cards
+      reset();
+
+		Card* temp = cards[Size - 1]; //Store card to be removed
+		cards.pop_back();	//Remove card from container
+		Size--;		//Decrement size of container
+		return temp; //Return removed card
+	}
+    void reset()
+    {
+        Size = OriginalSize;
+        for (int i = 0; i < Size; i++)
+        {
+			      cards.push_back(new Card(i)); //Fills deck with uninitialized cards
+        } 
+        Fill();
+        Shuffle(); //ReShuffle the deck
+    }
+    void Shuffle()
+    {
+      int j;
+      srand(time(0)); 
+       for (int i=0; i<size() ;i++) 
+        { 
+        // Random for remaining positions. 
+         j = (rand() % size()); 
+        Card* temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+        } 
+    }
+    
+
+    void PrintDeck()//Print 13 cards on one line
+    {
+       int counter = 0;
+       Sort(); //Sort the deck to print
+      while(counter < size())
+      {
+      //Print out all cards by rows of 13, each row is one suit
+      if(size() - counter > 12)
+      {
+          for(int j = 0; j < 13; j++)
+          {
+                cout << ".-----. ";
+          }
+            cout << endl;
+          for(int j = 0; j < 13; j++)
+          {
+                cout << "|" << cards[j + counter]->getRank() << ".-. | ";
+          }
+            cout << endl;
+            for(int j = 0; j < 13; j++)
+            {
+                cout << cards[j + counter]->cardMiddle(cards[j + counter]->getSuit()) << ' ';
+            }
+            cout << endl;
+            for(int j = 0; j < 13; j++)
+            {
+                cout << "| '-'" << cards[j + counter]->getRank() << "| ";
+            }
+            cout << endl;
+            for(int j = 0; j < 13; j++)
+            {
+                cout << "`-----' ";
+            }
+            cout<< endl;
+            counter += 13;
+       }
+        //end of more than 5 if statement
+      else
+      {
+        for(int i = counter; i < size(); i++)
+          {
+                cout << ".-----. ";
+          }
+            cout << endl;
+        for(int i = counter; i < size(); i++)
+          {
+                cout << "|" << cards[i]->getRank() << ".-. | ";
+          }
+            cout << endl;
+        for(int i = counter; i < size(); i++)
+            {
+                cout << cards[i-1]->cardMiddle(cards[i]->getSuit()) << ' ';
+            }
+            cout << endl;
+        for(int i = counter; i < size(); i++)
+            {
+                cout << "| '-'" << cards[i]->getRank() << "| ";
+            }
+            cout << endl;
+        for(int i = counter; i < size(); i++)    
+            {
+                cout << "`-----' ";
+            }
+            cout<< endl;
+            counter = size();
+      }
+    }
+    Shuffle(); //Reshuffle the deck after printing it
+  }
+};
